06cfb97f10953c48fc0e8f65e336678c4667b5a7
ryanmakori <ryanndigitalsolutionz@gmail.com>
2026-09-01 12:15:07 +0300

    feat: reset dev files structure

--- farmart-backend/config.py
+++ farmart-backend/config.py
@@ -8,3 +8,13 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///farmart.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    GOOGLE_REDIRECT_URL = os.getenv("GOOGLE_REDIRECT_URL")
+    SMTP_USERNAME = os.getenv("SMTP_USERNAME")
+    SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+    SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
+    SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "True").lower() == "true"
+    MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME", "Farmart")
+    MAIL_FROM_ADDRESS = os.getenv("MAIL_FROM_ADDRESS")
