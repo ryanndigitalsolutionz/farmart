f09d2ee0802914d9c3f8ff19e29b2bf2be47943c
Faith Kamande <faithkamande029@gmail.com>
2026-09-01 12:38:57 +0300

    checkout

--- src/pages/buyer/Checkout.jsx
+++ src/pages/buyer/Checkout.jsx
@@ -1,6 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-
+import { useCart } from "../../context/CartContext";
+import CheckoutSteps from "./CheckoutSteps";
+import OrderConfirmation from "./OrderConfirmation";
+import Delivery from "../delivery/Delivery";
 
 function Checkout() {
     const { cart, clearCart } = useCart();
@@ -8,6 +11,7 @@ function Checkout() {
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
     const [location, setLocation] = useState("");
+    const [orderNote, setOrderNote] = useState("");
     // information error 
     const [error, setError] = useState("");
 
@@ -15,6 +19,11 @@ function Checkout() {
     const [success, setSuccess] = useState("");
 
     const navigate = useNavigate();
+
+    const [deliverMethod, setDeliveryMethod] = useState("standard");
+    
+
+    const [currentStep, setCurrentStep] = useState(1);
   
     // calculate total in shopping cart
     const total = cart.reduce(
@@ -40,8 +49,13 @@ function Checkout() {
             buyer: {
                 name,
                 phone,
-                location,
             },
+            delivery: {
+                location,
+                method: deliverMethod,
+                note: "orderNote",
+            },
+            
             items: cart,
             total,
             status: "pending",
@@ -66,82 +80,94 @@ function Checkout() {
 
         console.log("Order saved:", order);
 
-        navigate("/order-confirmation", {
-            state: { orderId: order.id},
-        });
+        navigate(`/payments/${order.id}`);
     };    
 
   return (
-    <div className="p-4">
-        <h1 className="font-bold text-2xl text-center tracking-wide">Checkout</h1>
-
-        <h2 className="font-semibold">Order Summary</h2>
-
-        {cart.map((animal) => (
-            <div key={animal.id}>
-                <p>{animal.name}</p>
-                <p>
-                    Ksh {Number(animal.price).toLocaleString()}
-                </p>                
-            </div>
-        ))}
-
-        <h2>Total: ksh {total.toLocaleString()}</h2>
-        <h2>Buyer information</h2>
-
-        {/* buyer information inputs */}
+    <div className="p-4 max-w-3xl mx-auto shadow-2xl m-3 items-center justify-center flex flex-col gap-4">
         <div className="">
-            <label>
-                Full Name
-                <input 
-                    type="text" 
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter your full name"
-                    className=""
-                    required
-                />
-            </label>
-        </div>
-        <div className="">
-            <label>
-                Phone Number
-                <input 
-                    type="text" 
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    placeholder="Enter your phone number"
-                    className=""
-                    required
-                />
-            </label>
-        </div>
-        <div className="">
-            <label>
-                Location
-                <input 
-                    type="text" 
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Enter your Location"
-                    className=""
-                    required
-                />
-            </label>
-        </div>
+            <h1 className="font-bold text-2xl text-center tracking-wide mb-2">
+                Checkout
+            </h1>
+            <CheckoutSteps currentStep={currentStep} />
 
-        {error && <p>{error}</p>}
 
-        <button 
-            onClick={handlePlaceOrder} 
-            disabled={submitting}
-            className="text-green-600 rounded-lg mt-2 font-semibold"
-        >
-            {submitting ? "Placing order" : "Place Order"}
-        </button>
+        </div>
+        <div className="flex flex-col justify-center items-center mt-5">
+            {currentStep === 1 && (
+                <div>
+                    <h2 className="font-bold  mb-5 tracking-wide text-xl text-gray-700 text-center p-2">Order Summary</h2>
+                    <OrderConfirmation />
+                
+
+                {cart.map((animal) => (
+                    <div key={animal.id} className="flex flex-col gap-1">                
+                        <div className="flex gap-3">
+                            <img 
+                                src={animal.images} 
+                                alt={animal.type} 
+                                className="w-35 h-35 object-cover mb-4"
+                            />
+                            <div className="flex flex-col gap-1">
+                                <p className="font-bold text-xl ">{animal.breed} {animal.type}</p>                                
+                                <p className="text-gray-400">{animal.age} years - {animal.weight} kg</p>
+                                <p className="text-gray-400">{animal.location}</p>
+                                <p className="font-semibold">
+                                Price: Ksh {Number(animal.price).toLocaleString()}
+                                </p>
+                            </div>  
+                        </div>              
+                    </div>
+                ))}
+
+                <h2 className="font-bold text-xl mt-3 text-green-700">Total: ksh {total.toLocaleString()}</h2>
+
+                <div className="flex gap-4 mt-3 mb-4">
+                    <button 
+                            onClick={() => setCurrentStep(2)}
+                            className="text-green-600 font-semibold border-3 p-2 px-4 py-1 rounded-lg hover:bg-green-500 hover:text-white"
+                        >
+                            Continue to payment
+                    </button>
+                    <Link 
+                        to="/marketplace"
+                        className="bg-green-600 px-2 py-1 rounded-lg  text-white font-semibold hover:bg-green-900 "
+                    >
+                    Continue Shopping
+                    </Link>
+                </div>
+
+                </div>
+            )}
+
+            {currentStep === 2 && (
+                <Delivery
+                    name={name}
+                    setName={setName}
+                    phone={phone}
+                    setPhone={setPhone}
+                    location={location}
+                    setLocation={setLocation}
+                    orderNote={orderNote}
+                    setOrderNote={setOrderNote}
+                    error={error}
+                    submitting={submitting}
+                    onContinue={handlePlaceOrder}
+                />
+            )}
+
+            {currentStep === 3 && (
+                
+                <Payments
+                    total={total}
+                    onBack={() => setCurrentStep(2)}
+                    onPlaceOrder={handlePlaceOrder}
+                />
+            )}
+        </div>
 
     </div>
   )
-}
+}   
 
-export default Checkout
+export default Checkout;
