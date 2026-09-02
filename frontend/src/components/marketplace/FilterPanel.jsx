4ed276e6d94e7ac83c005323a0421abd91d8083a
Faith Kamande <faithkamande029@gmail.com>
2026-09-02 14:24:21 +0300

    package json

--- frontend/src/components/marketplace/FilterPanel.jsx
+++ frontend/src/components/marketplace/FilterPanel.jsx
@@ -19,7 +19,9 @@ function FilterPanel({
   setMaxPrice,
 }) {
   return (
-    <div className="filter-panel flex flex-wrap overflow-hidden gap-2 items-center p-4 border-2 rounded-2xl border-(--farm-green-border) w-full">
+    <div 
+      className="filter-panel flex flex-wrap overflow-hidden 
+      gap-4 items-center p-4  border-2 rounded-2xl border-(--farm-green-border)  w-full">
 
       <LivestockTypeFilter
         type={type}
